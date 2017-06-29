46819b134e120d5d0bf0e013ae859029e1bc14b9
wzy <wen_zhouyang@163.com>
2017-06-29 18:03:41 +0800

    redis 执行lua

--- spring-boot-redis1/src/main/java/com/wzy/springboot/rexxar/redis1/RedisAutoConfig.java
+++ spring-boot-redis1/src/main/java/com/wzy/springboot/rexxar/redis1/RedisAutoConfig.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * Created by wzy on 2017/6/28.
@@ -16,17 +17,20 @@ import org.springframework.data.redis.core.script.RedisScript;
 public class RedisAutoConfig {
 
     @Bean
-    public RedisScript<Long> redisScript(RedisProperties redisProperties) {
-        DefaultRedisScript<Long> script = new DefaultRedisScript<>();
-        script.setResultType(Long.class);
+    public RedisScript<String> redisScript(RedisProperties redisProperties) {
+        DefaultRedisScript<String> script = new DefaultRedisScript<>();
+        script.setResultType(String.class);
         script.setLocation(redisProperties.getTest1());
         return script;
     }
 
-    @Bean("abc")
+    @Bean
     public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setStringSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         return redisTemplate;
     }
 }
--- spring-boot-redis1/src/test/java/com/wzy/springboot/rexxar/redis1/Test.java
+++ spring-boot-redis1/src/test/java/com/wzy/springboot/rexxar/redis1/Test.java
@@ -3,11 +3,19 @@ package com.wzy.springboot.rexxar.redis1;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.ReturnType;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import redis.clients.util.SafeEncoder;
 
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by wzy on 2017/6/28.
@@ -17,15 +25,32 @@ import java.util.Collections;
 public class Test {
 
     @Autowired
-    private RedisScript redisScript;
+    private RedisScript<String> redisScript;
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisTemplate<String, String> redisTemplate;
 
     @org.junit.Test
-    public void name() throws Exception {
+    public void test() throws Exception {
         System.out.println(redisScript.getSha1());
-        Long test1 = (Long) redisTemplate.execute(redisScript, Collections.singletonList("test"), 1, 50);
-        System.out.println(test1);
+        List<String> keys = Collections.singletonList("test123");
+        String test123 = redisTemplate.execute(redisScript, new StringRedisSerializer(), new StringRedisSerializer(),
+                keys, "1", "20");
+
+
+        System.out.println(test123);
     }
+
+    @org.junit.Test
+    public void test1() throws Exception {
+        redisTemplate.execute((RedisCallback<Object>) connection -> {
+            Object o = connection.evalSha("91cb7bdd1fafe24eeb2c0b487dcca65fe670c7c2", ReturnType.INTEGER, 1,
+                    "test"
+                    .getBytes(), "1".getBytes(), "20".getBytes());
+            System.out.println(o);
+            return o;
+        });
+    }
+
+
 }
